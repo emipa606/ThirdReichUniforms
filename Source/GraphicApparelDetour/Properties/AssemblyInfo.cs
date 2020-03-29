f7dabc1fccc793351acc4aaca974c03f5378b705
Mlie <emipa606@users.noreply.github.com>
2020-03-29 22:05:41 +0200

    First publish

---
+++ Source/GraphicApparelDetour/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyVersion("1.19.0.0")]
+[assembly: AssemblyTitle("RimWorldCommunity")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GraphicApparelDetour")]
+[assembly: AssemblyCopyright("Copyright ©  2018 Mrofa - Jecrell")]
+[assembly: AssemblyTrademark("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("b8734eda-63e8-4b51-872a-863c2bc3d90e")]
+[assembly: AssemblyFileVersion("1.19.0.0")]
